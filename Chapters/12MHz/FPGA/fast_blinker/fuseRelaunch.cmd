0319cf56e076f6f993ea963748c3f693b26653a2
XESS Corp <xesscorp@xess.com>
2011-07-10 10:27:27 -0400

    Added chapter on using DFS for clock synthesis.

---
+++ Chapters/12MHz/FPGA/fast_blinker/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -o "C:/xesscorp/PRODUCTS/TUTORIALS/FpgasNowWhat/Chapters/12MHz/FPGA/fast_blinker/fast_blinkerTestBench_isim_beh.exe" -prj "C:/xesscorp/PRODUCTS/TUTORIALS/FpgasNowWhat/Chapters/12MHz/FPGA/fast_blinker/fast_blinkerTestBench_beh.prj" "work.fast_blinkerTestBench" 
